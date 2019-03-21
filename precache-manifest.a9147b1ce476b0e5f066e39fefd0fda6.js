0f56f61b0454861eed159fb48b3aa9ac23ac621d
AlainBenitez <abenitezrodrigue@student.fullsail.edu>
2019-03-21 10:50:29 -0400

    Updates

---
+++ precache-manifest.a9147b1ce476b0e5f066e39fefd0fda6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f052cec30c61079bb11",
+    "url": "/Portfolio5/static/js/runtime~main.7e954230.js"
+  },
+  {
+    "revision": "b268ee4ff563d0718a51",
+    "url": "/Portfolio5/static/js/main.31cd97d0.chunk.js"
+  },
+  {
+    "revision": "75147420762d8cdd08d0",
+    "url": "/Portfolio5/static/js/2.1801e145.chunk.js"
+  },
+  {
+    "revision": "b268ee4ff563d0718a51",
+    "url": "/Portfolio5/static/css/main.f15bd92e.chunk.css"
+  },
+  {
+    "revision": "e572b9dc8af75cd986913e55fd14c81b",
+    "url": "/Portfolio5/index.html"
+  }
+];
